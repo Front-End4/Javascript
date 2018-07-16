e25c14665c5776ebeba785f9d857bb6ecc641e61
Alexander Vasuluev <alexhateful@gmail.com>
2018-07-16 19:51:39 +0300

    add lesson 01

---
+++ les01/js/scripts.js
@@ -0,0 +1,19 @@
+// let aalert = alert('hello!');
+// let cconfirm = confirm('да или нет?');
+// let a = prompt('введите первое число:');
+// let b = prompt('введите второе число:');
+
+// var aaa = 1;
+// let bbb = 2;
+// const ccc = 3;
+
+// console.log('hello!');
+// console.info('никита учится');
+// console.error('никита не учится');
+
+// types
+// let a = 1; // any number
+// let b = "s"; // any string
+// let c = true; // true of false
+// let d = null; // null
+// let f = undefined; // undefined
