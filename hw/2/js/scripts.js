a0cb66a8734f14d72c0c8820f0025360ab261902
Alexander Vasuluev <alexhateful@gmail.com>
2018-07-18 20:20:24 +0300

    123

---
+++ hw/2/js/scripts.js
@@ -0,0 +1,11 @@
+let symbol = '2',
+    cols = 2,
+    rows = 2,
+    string ='',
+    sum = 0;
+
+for (let index = 0; index < 10; index++) {
+    string = string + index;
+    console.log(index, string);
+    
+}
