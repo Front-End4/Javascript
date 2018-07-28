d24dfa99de687bd0b6e6959e11f1cbca10710a59
Alexander Vasuluev <alexhateful@gmail.com>
2018-07-28 16:19:51 +0300

    add simple navigation example

--- events/js/scripts.js
+++ events/js/scripts.js
@@ -1,11 +1,31 @@
-let first = document.getElementById("first");
+const menu = document.getElementById("menu"),
+        submenu = document.querySelectorAll(".submenu");
 
-first.hello = function() {
-    this.innerText = "hello";
+for (const element in submenu) {
+    if (submenu.hasOwnProperty(element)) {
+        let arrow = document.createElement("i");
+        arrow.innerText = ">";
+        const elem = submenu[element];
+        elem.classList.add("hide");
+        elem.parentElement.appendChild(arrow);
+    }
 }
+let state = false, lastElem;
 
-// console.log(first.hello());
-
-
-// first.property; first.property = 1;
-// first.method();
+menu.addEventListener("click", function () {
+    if (event.target.nodeName == "I") {
+        if (lastElem) {
+            lastElem.classList.add("hide");
+            state = false;
+        }
+        if (!state) {
+            lastElem = event.target.previousElementSibling;
+            lastElem.classList.remove("hide");
+            state = true;
+        }
+        else {
+            event.target.previousElementSibling.classList.add("hide");
+            state = false;
+        }
+    }
+})
