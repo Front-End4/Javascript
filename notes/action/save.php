f0060c0c503c86d1cb1ce021600514bf86ccb69a
Alexander Vasuluev <alexhateful@gmail.com>
2018-08-29 18:43:28 +0300

    add db connection

--- notes/action/save.php
+++ notes/action/save.php
@@ -3,3 +3,29 @@
 $fp = fopen('../data/data.txt', 'w');
 fwrite($fp, $_POST['data']);
 fclose($fp);
+
+$servername = "localhost";
+$username = "notes";
+$password = "notes";
+$dbname = "notes";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT publicationdate, ID, title, text FROM data";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "publicationdate: " . $row["publicationdate"]. " - ID: " . $row["ID"]. " title - " . $row["title"]. " text - " . $row["text"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
